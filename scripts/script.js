abff213db7c241e51505c119ca2acc23027fe0e7
Davi Silva Santos <morg.daviss@gmail.com>
2022-04-26 16:06:16 -0300

    a1

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+
+
+
+// document.getElementById('email').value.addEventListener("click", function(){
+//     document.getElementById('email').select();
+//     document.execCommand('copy');
+// }
+
+// );
+
+
+
+var icon = document.getElementById('icon');
+
+icon.onclick = function(){
+    document.body.classList.toggle('dark-theme')
+}
+
