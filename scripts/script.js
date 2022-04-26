b57fcf94830cb99b550cbacf8dbb08db76e3d8b8
Davi Silva Santos <morg.daviss@gmail.com>
2022-04-26 20:07:49 -0300

    fixed the download issue, fineshed the dark mode and the style of the page as well

--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,11 @@
 var icon = document.getElementById('icon');
 
 icon.onclick = function(){
-    document.body.classList.toggle('dark-theme')
+    document.body.classList.toggle('dark-theme');
+    if(document.body.classList.contains('dark-theme')){
+        icon.src = './assets/images/sun.png';
+    }else{
+        icon.src = './assets/images/moon.png';
+    }
 }
 
