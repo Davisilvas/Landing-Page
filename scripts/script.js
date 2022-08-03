0f59309f161c6bc4ae0e111f537981fed6bd79fc
Davi Silva Santos <morg.daviss@gmail.com>
2022-08-03 20:25:14 -0300

    saving at this point of the project before big remaking in projects section

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,8 @@
 
 
+// arr = [
+//     [1, 2, 3],
+//     [7, 8, 9]
+// ]
 
-
-var icon = document.getElementById('icon');
-
-icon.onclick = function(){
-    document.body.classList.toggle('dark-theme');
-    if(document.body.classList.contains('dark-theme')){
-        icon.src = './assets/images/sun.png';
-    }else{
-        icon.src = './assets/images/moon.png';
-    }
-}
-
+// console.log(arr[1][1]);
